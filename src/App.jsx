2095433c1f43968d5fb1719fd31a9b8c58bebeba
susri2024 <susrisangitabiswal97@gmail.com>
2025-02-12 17:55:17 +0530

    setting profile updated with profile and address page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 // import AppRoutes from './components/AppRouters'
 // import { ProductDropdown } from './components/dropdown/ProductDropdown'
 // import ProfileDetails from './components/SettingProfile/ProfileDetails/ProfileDetails'
-import ProfileSidebar from './components/SettingProfile/ProfileSidebar/ProfileSidebar'
+// import ProfileSidebar from './components/SettingProfile/ProfileSidebar/ProfileSidebar'
+// import ProfileOrder from './components/SettingProfile/ProfilePages/ProfileOrder/ProfileOrder'
+import ProfileAddress from './components/SettingProfile/ProfilePages/ProfileAddress/ProfileAddress'
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
     {/* <AppRoutes /> */}
     {/* <ProductDropdown/> */}
     {/* <ProfileDetails/> */}
-    <ProfileSidebar/>
+    {/* <ProfileSidebar/> */}
+    {/* <ProfileOrder/> */}
+    <ProfileAddress/>
     </>
   )
 }
--- src/components/SettingProfile/ProfileSidebar/ProfileSidebar.jsx
+++ src/components/SettingProfile/ProfileSidebar/ProfileSidebar.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import "./ProfileSidebar.css"
-import { Col, Container, Row, Card, ListGroup } from 'react-bootstrap'
+import React from "react";
+import "./ProfileSidebar.css";
+import { Col, Container, Row, Card, ListGroup } from "react-bootstrap";
 
 const ProfileSidebar = () => {
   return (
-<Container>
-    <Row>
+    <Container>
+      <Row>
         <Col md={3}>
-         <Card className="sidebar-card">
-         ACTIVITY
-            <Card.Body className="sidebar-list">
-              <ListGroup  >
-                <ListGroup.Item>Profile</ListGroup.Item>
-                <ListGroup.Item>Order History</ListGroup.Item>
-                <ListGroup.Item>My Address</ListGroup.Item>
-              </ListGroup>
-            </Card.Body>
-          </Card>
+          <div className="sidebar-card">
+            <div className="sidebar-heading"> ACTIVITY </div>
+            <div className="sidebar-list">
+              <button className="sidebar-button1">Profile</button>
+              <button className="sidebar-button1">Order History</button>
+              <button className="sidebar-button1">My Addresses</button>
+            </div>
+          </div>
         </Col>
-    </Row>
-</Container>
-)
-}
+      </Row>
+    </Container>
+  );
+};
 
-export default ProfileSidebar
+export default ProfileSidebar;
