c8ea65da0b3d1409305eba1c6933e11d7b06ef76
SHIBANI MOHANTY <140004942+shibanimohant@users.noreply.github.com>
2025-02-12 17:55:18 +0530

    productsidebar creat

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
+import ProductSidebar from './components/ProductSidebar/ProductSidebar'
 // import AppRoutes from './components/AppRouters'
 // import Login from './components/Login/Login'
-import Registration from './components/Registration/Registration'
+// import Registration from './components/Registration/Registration'
 
 function App() {
   return (
     <>
     {/* <AppRoutes /> */}
     {/* <Login /> */}
-    <Registration />
+    {/* <Registration /> */}
+    <ProductSidebar/>
     </>
   )
 }
