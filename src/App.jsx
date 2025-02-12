80d86c0476a42c489bb4c761242029a1f5f22431
Nibedita Bal <nibeditabal2001@gmail.com>
2025-02-12 16:27:03 +0530

    pull from dev...

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 // import AppRoutes from './components/AppRouters'
 import Footer from './components/Footer/Footer'
 import LowerProducts from './components/LowerProducts/LowerProducts'
-
+import ProductGrid from './components/Items/ProductGrid/ProductGrid'
 function App() {
   return (
     <>
     {/* <AppRoutes /> */}
     {/* <LowerProducts/> */}
     {/* <Footer/> */}
+    <ProductGrid/>
     </>
   )
 }
