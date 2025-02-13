3cf04e917cd40d92df3000b2e1ca00f7ba83b48a
@hacker <129648421+hackerpapun@users.noreply.github.com>
2025-02-13 09:37:47 +0530

    create cpmpletre home page styling is still not fixed just added and design the components ....

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 ﻿import React from 'react'
 import { HomeCarasole } from '../components/HomeCarasole/HomeCarasole'
+import { HomeProducts } from '../components/HomeProducts/HomeProducts'
 
 export const Home = () => {
   return (
     <div>
       <HomeCarasole/>
+      <HomeProducts/>
     </div>
   )
 }
