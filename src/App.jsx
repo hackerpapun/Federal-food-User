66f15ff5ba13180302283e631b7cd260a32edec1
susri2024 <susrisangitabiswal97@gmail.com>
2025-02-14 14:41:04 +0530

    dropdow updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css'
-// import AppRoutes from './components/AppRouters'
+import AppRoutes from './components/AppRouters'
 // import { ProductDropdown } from './components/dropdown/ProductDropdown'
 // import ProfileDetails from './components/SettingProfile/ProfileDetails/ProfileDetails'
 // import ProfileSidebar from './components/SettingProfile/ProfileSidebar/ProfileSidebar'
 // import ProfileOrder from './components/SettingProfile/ProfilePages/ProfileOrder/ProfileOrder'
 // import ProfileAddress from './components/SettingProfile/ProfilePages/ProfileAddress/ProfileAddress'
-import Addresslocation from './components/SettingProfile/ProfilePages/ProfileAddress/Addresslocation'
+// import Addresslocation from './components/SettingProfile/ProfilePages/ProfileAddress/Addresslocation'
 
 export const App = () => {
   return (
     <>
-    {/* <AppRoutes /> */}
+    <AppRoutes />
     {/* <ProductDropdown/> */}
     {/* <ProfileDetails/> */}
     {/* <ProfileSidebar/> */}
     {/* <ProfileOrder/> */}
     {/* <ProfileAddress/> */}
-    <Addresslocation/>
+    {/* <Addresslocation/> */}
     </>
   )
 }
--- src/components/SettingProfile/ProfilePages/ProfileAddress/Addresslocation.jsx
+++ src/components/SettingProfile/ProfilePages/ProfileAddress/Addresslocation.jsx
@@ -1,18 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Addresslocation.css";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Modal, Row, Button } from "react-bootstrap";
 
 export const Addresslocation = () => {
+  function LocationModal() {
+    const [show, setShow] = useState(true);
+    const [address, setAddress] = useState(
+      "9RCM+GWR, Raghunathpur, Bhubaneswar, 751024, India"
+    );
+
+    const handleClose = () => setShow(false);
+    const handleChangeAddress = () => {
+      alert("Implement address change functionality here!");
+    };
+
+    return (
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        className="location-modal-container"
+      >
+        <Modal.Header></Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Col>
+              <div id="map">
+                <p>set the map here</p>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="location-heading">Set your delivery location</Col>
+          </Row>
+          <Row>
+            <Col className="delivery-heading">
+            DELIVERY AREA
+            </Col>
+            <Col>
+              <div className="delivery-address">
+                {address}{" "}
+                <span className="change-address" onClick={handleChangeAddress}>
+                  Change
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Modal.Body>
+
+
+        <Modal.Footer className="footer-buttons">
+          <Button className="confirm-button" variant="primary" onClick={handleClose}>
+            CONFIRM AND PROCEED
+          </Button>
+          <Button className="cancel-button" variant="secondary" onClick={handleClose}>
+            CANCEL
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
   return (
-    <Container>
-      <Row>
-        <Col className="location-area">
-        location
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <LocationModal />
+    </div>
   );
 };
 
 export default Addresslocation;
-
