48a80d10a175afc3e6d5568e9f37f15198d9d4f0
SHIBANI MOHANTY <140004942+shibanimohant@users.noreply.github.com>
2025-02-20 16:50:06 +0530

    applycoupon page update

--- src/components/Applycoupon/Applycoupon.jsx
+++ src/components/Applycoupon/Applycoupon.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Applycoupon.css";
-import { Button, Col, Modal, ModalHeader, Row, ModalBody, ModalTitle } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Modal,
+  ModalHeader,
+  Row,
+  ModalBody,
+  ModalTitle,
+  ModalFooter,
+} from "react-bootstrap";
 
-const Applycoupon = ({ show, onHide }) => {
+const Applycoupon = () => {
+  const [setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <Modal show={show} onHide={onHide} centered className="couponpage">
-      <ModalHeader>
+    <Modal show={handleShow} centered className="couponpage">
+      <ModalHeader className="hcoupon">
         <ModalTitle className="tcoupon">Apply Coupon</ModalTitle>
       </ModalHeader>
-      <ModalBody>
+      <ModalBody className="bcoupon"></ModalBody>
+      <ModalFooter>
         <Row
           className="text-center"
           style={{
@@ -18,13 +32,20 @@ const Applycoupon = ({ show, onHide }) => {
           }}
         />
         <Row>
-          <Col className="text-center">
-            <Button className="cancelbtn" type="button" onClick={onHide}>
+          <Col
+            className="text-center"
+            style={{
+              display: "flex",
+              justifyItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Button className="cancelbtn" type="button" onClick={handleClose}>
               Cancel
             </Button>
           </Col>
         </Row>
-      </ModalBody>
+      </ModalFooter>
     </Modal>
   );
 };
