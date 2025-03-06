ba8da1b389ec3e5019cf63a3cf10a51409cf0c61
SHIBANI MOHANTY <140004942+shibanimohant@users.noreply.github.com>
2025-03-06 16:12:36 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 // import AppRoutes from "./components/AppRouters.jsx";
 // import Applycoupon from "./components/Applycoupon/Applycoupon";
 // import ProfileAddress from "./components/SettingProfile/ProfilePages/ProfileAddress/ProfileAddress.jsx";
 // import OrderSummery from "./components/AddtoCart/OrderSummery.jsx";
-// import AddCart from "./components/AddtoCart/Addcart.jsx";
-// import Orderhistory from "./components/SettingProfile/ProfilePages/ProfileOrder/Orderhistory.jsx";
-// import Orderhistorydetails from "./components/SettingProfile/ProfilePages/ProfileOrder/Orderhistorydetails.jsx";
-// import Payment from "./components/AddtoCart/Payment.jsx";
-// import PaymentPage from "./components/AddtoCart/PaymentPage.jsx";
-// import ConfirmOrder from "./components/confirmOrder/ConfirmOrder.jsx";
-import ChatModal from "./components/SettingProfile/ProfileChat/ChatModal.jsx";
+// // import AddCart from "./components/AddtoCart/Addcart.jsx";
+// // import Orderhistory from "./components/SettingProfile/ProfilePages/ProfileOrder/Orderhistory.jsx";
+// // import Orderhistorydetails from "./components/SettingProfile/ProfilePages/ProfileOrder/Orderhistorydetails.jsx";
+// // import Payment from "./components/AddtoCart/Payment.jsx";
+// // import PaymentPage from "./components/AddtoCart/PaymentPage.jsx";
+// // import ConfirmOrder from "./components/confirmOrder/ConfirmOrder.jsx";
+// import ChatModal from "./components/SettingProfile/ProfileChat/ChatModal.jsx";
 
-function App() {
-  return (
-    <>
-    {/* <AddCart/> */}
-      {/* <AppRoutes /> */}
-      {/* <ProfileAddressBar/> */}
-      {/* <Applycoupon /> */}
-      {/* <ProfileAddress/> */}
-      {/* <Orderhistorydetails/> */}
-      {/* <OrderTracking/> */}
-      {/* <Orderhistory/> */}
-      {/* <ConfirmOrder/> */}
-      <ChatModal/>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//     {/* <AddCart/> */}
+//       {/* <AppRoutes /> */}
+//       {/* <ProfileAddressBar/> */}
+//       {/* <Applycoupon /> */}
+//       {/* <ProfileAddress/> */}
+//       {/* <Orderhistorydetails/> */}
+//       {/* <OrderTracking/> */}
+//       {/* <Orderhistory/> */}
+//       {/* <ConfirmOrder/> */}
+//       <ChatModal/>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
 
 
 
